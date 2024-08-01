1bd7fe54174fb3231427b029c78e9862f3023519
jodaki <jodakianm@gmail.com>
2024-08-01 15:29:49 +0330

    slider edit

---
+++ js/app.js
@@ -0,0 +1,29 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    loop : true,
+    centeredSlides: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 50
+        },
+    },
+    on: {
+        slideChange: function () {
+            this.slides.forEach((slide, index) => {
+                if (index === this.activeIndex) {
+                    slide.style.transform = 'scale(1.5)';
+                    slide.style.zIndex = '10';
+                } else {
+                    slide.style.transform = 'scale(1)';
+                    slide.style.zIndex = '1';
+                }
+            });
+        }
+    }
+});
